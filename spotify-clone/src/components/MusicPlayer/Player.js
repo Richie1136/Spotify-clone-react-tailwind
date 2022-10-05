b76ae53061a5b10c0f813fc52eb402410cb34e5c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-05 13:36:45 -0400

    Built out the Player component

--- spotify-clone/src/components/MusicPlayer/Player.js
+++ spotify-clone/src/components/MusicPlayer/Player.js
@@ -1,7 +1,30 @@
+import { useRef, useEffect } from 'react'
 
-const Player = ({ activeSong, volume, isPlaying, seektime, repeat, currentIndex, onEnded, onTimeUpdate, onLoadedData }) => {
+
+const Player = ({ activeSong, volume, isPlaying, seektime, repeat, onEnded, onTimeUpdate, onLoadedData }) => {
+
+  const ref = useRef(null)
+
+  if (ref.current) {
+    if (isPlaying) {
+      ref.current.play()
+    } else {
+      ref.current.pause()
+    }
+  }
+
+  useEffect(() => {
+    ref.current.volume = volume
+  }, [volume])
+
+  useEffect(() => {
+    ref.current.currentTime = seektime
+  }, [seektime])
+
+
+
   return (
-    <div>Player</div>
+    <audio src={activeSong?.hub?.actions[1].uri} ref={ref} loop={repeat} onEnded={onEnded} onTimeUpdate={onTimeUpdate} onLoadedData={onLoadedData} />
   )
 }
 
