3bba32a80c15004df3d9dcfabcf9d42ce9fed15e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-05 13:50:22 -0400

    Built out the SeekBar component

--- spotify-clone/src/components/MusicPlayer/SeekBar.js
+++ spotify-clone/src/components/MusicPlayer/SeekBar.js
@@ -1,7 +1,20 @@
 
 const SeekBar = ({ value, min, max, onInput, setSeekTime, apptime }) => {
+
+  const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`
+
   return (
-    <div>SeekBar</div>
+    <div className="hidden sm:flex flex-row items-center">
+      <button className="hidden lg:mr-4 lg:block text-white" type="button" onClick={() => setSeekTime(apptime - 1)}>
+        -
+      </button>
+      <p className="text-white">{value === 0 ? '0:00' : getTime(value)}</p>
+      <input className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg" type="range" step="any" value={value} min={min} max={max} onInput={onInput} />
+      <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
+      <button className="hidden lg:ml-4 lg:block text-white" type="button" onClick={() => setSeekTime(apptime + 5)}>
+        +
+      </button>
+    </div>
   )
 }
 
