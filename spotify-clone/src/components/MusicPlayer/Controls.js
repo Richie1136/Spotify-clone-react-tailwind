54e5a60b7220b087ab196c9f13b2fa7aa03ddff1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-05 14:36:30 -0400

    created a index.js file inside of the pages folder to put all of my pages file inside of and created routes for all of my pages

--- spotify-clone/src/components/MusicPlayer/Controls.js
+++ spotify-clone/src/components/MusicPlayer/Controls.js
@@ -1,20 +1,18 @@
 import { MdSkipNext, MdSkipPrevious } from 'react-icons/md'
 import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs'
 
-const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }) => {
-  return (
-    <div className='flex items-center justify-around md:w-16 lg:w-52 2xl:w-80'>
-      <BsArrowRepeat className='hidden sm:block cursor-pointer' size={20} color={repeat ? 'red' : 'white'} onClick={() => setRepeat((prev) => !prev)} />
-      {currentSongs?.length && <MdSkipPrevious className='cursor-pointer' size={30} color="#FFF" onClick={handlePrevSong} />}
-      {isPlaying ? (
-        <BsFillPauseFill className='cursor-pointer' size={45} color="#FFF" onClick={handlePlayPause} />
-      ) : (
-        <BsFillPlayFill className='cursor-pointer' size={45} color="#FFF" onClick={handlePlayPause} />
-      )}
-      {currentSongs?.length && <MdSkipNext className='cursor-pointer' size={30} color="#FFF" onClick={handleNextSong} />}
-      <BsShuffle className='hidden sm:block cursor-pointer' size={20} color={shuffle ? 'red' : 'white'} onClick={() => setShuffle((prev) => !prev)} />
-    </div>
-  )
-}
+const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }) => (
+  <div className='flex items-center justify-around md:w-16 lg:w-52 2xl:w-80'>
+    <BsArrowRepeat className='hidden sm:block cursor-pointer' size={20} color={repeat ? 'red' : 'white'} onClick={() => setRepeat((prev) => !prev)} />
+    {currentSongs?.length && <MdSkipPrevious className='cursor-pointer' size={30} color="#FFF" onClick={handlePrevSong} />}
+    {isPlaying ? (
+      <BsFillPauseFill className='cursor-pointer' size={45} color="#FFF" onClick={handlePlayPause} />
+    ) : (
+      <BsFillPlayFill className='cursor-pointer' size={45} color="#FFF" onClick={handlePlayPause} />
+    )}
+    {currentSongs?.length && <MdSkipNext className='cursor-pointer' size={30} color="#FFF" onClick={handleNextSong} />}
+    <BsShuffle className='hidden sm:block cursor-pointer' size={20} color={shuffle ? 'red' : 'white'} onClick={() => setShuffle((prev) => !prev)} />
+  </div>
+)
 
 export default Controls
---
+++ spotify-clone/src/pages/index.js
@@ -0,0 +1,9 @@
+import Discover from './discover/Discover'
+import TopArtists from './topartists/TopArtists'
+import ArtistDetails from './artistdetails/ArtistDetails'
+import SongDetails from './songdetails/SongDetails'
+import Search from './search/Search'
+import TopCharts from './topcharts/TopCharts'
+import AroundYou from './aroundyou/AroundYou'
+
+export { Discover, Search, TopArtists, ArtistDetails, SongDetails, TopCharts, AroundYou }
