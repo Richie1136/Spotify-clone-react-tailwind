a925af25ba00d7d914c358243886a41b2e63ef8c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-06 10:56:53 -0400

    Added the TopCharts endpoint

--- spotify-clone/src/redux/services/shazamCore.js
+++ spotify-clone/src/redux/services/shazamCore.js
@@ -8,6 +8,10 @@ export const shazamCoreApi = createApi({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-Key', 'e9f136fd86msh20374d953bfb05ep137d7ejsnaede58975cae')
+      return headers
     },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: '/charts/world' }),
   })
 })
