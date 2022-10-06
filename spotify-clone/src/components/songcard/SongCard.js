7c3c7b65aae4fef6ccfc3d2d05230fe65283b88d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-06 11:39:58 -0400

    Styling the parent div inside of the SongCard component

--- spotify-clone/src/components/songcard/SongCard.js
+++ spotify-clone/src/components/songcard/SongCard.js
@@ -4,9 +4,9 @@ import { playPause, setActiveSong } from "../../redux/features/playerSlice"
 import PlayPause from '../playpause/PlayPause'
 
 
-const SongCard = ({ song }) => {
+const SongCard = ({ song, index }) => {
   return (
-    <div>
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <h2>{song.title}</h2>
     </div>
   )
