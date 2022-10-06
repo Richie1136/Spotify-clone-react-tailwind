0ca078fd98065c44c852fdd4541a61efa72b5c7a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-06 11:52:10 -0400

    Added more styling and added the playpause component

--- spotify-clone/src/components/songcard/SongCard.js
+++ spotify-clone/src/components/songcard/SongCard.js
@@ -5,9 +5,17 @@ import PlayPause from '../playpause/PlayPause'
 
 
 const SongCard = ({ song, index }) => {
+
+  const activeSong = "test"
+
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <h2>{song.title}</h2>
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+          <PlayPause />
+        </div>
+        <h2>{song.title}</h2>
+      </div>
     </div>
   )
 }
