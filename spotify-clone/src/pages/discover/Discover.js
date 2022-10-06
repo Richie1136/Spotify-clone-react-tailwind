ea0e37e5f4cd2daee55a81754d75b3af1cb33bc9
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-06 10:18:21 -0400

    Mapping over all genres and rendering out all of the genres as options

--- spotify-clone/src/pages/discover/Discover.js
+++ spotify-clone/src/pages/discover/Discover.js
@@ -3,8 +3,20 @@ import { genres } from '../../assets/constants'
 
 
 const Discover = () => {
+
+  const genreTitle = "Pop"
+
   return (
-    <div>Discover</div>
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+        <select className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5" onChange={() => { }} value="">
+          {genres.map((genre) => (
+            <option>{genre.title}</option>
+          ))}
+        </select>
+      </div>
+    </div>
   )
 }
 
