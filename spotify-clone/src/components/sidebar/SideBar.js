aad12a351e03e0559dfe55d3f38ad3d8d885e39a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-10 11:56:06 -0400

    Added onclick property to the 2 icons to open and close the mobile menu

--- spotify-clone/src/components/sidebar/SideBar.js
+++ spotify-clone/src/components/sidebar/SideBar.js
@@ -17,7 +17,12 @@ const SideBar = () => {
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
-        {isMobileMenuOpen ? <RiCloseLine /> : <HiOutlineMenu />}
+        {isMobileMenuOpen ? <RiCloseLine onClick={() => setIsMobileMenuOpen(false)} className="w-6 h-6 text-white mr-2" /> : <HiOutlineMenu onClick={() => setIsMobileMenuOpen(true)} className="h-6 w-6 text-white mr-2" />}
+      </div>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] 
+      backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${isMobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img className="w-full h-14 object-contain" src={musicLogo} alt='logo' />
+        <NavLinks handleClick={() => setIsMobileMenuOpen(false)} />
       </div>
     </>
   )
