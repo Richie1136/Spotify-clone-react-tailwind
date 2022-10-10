7f2fff8cf6be1ad5a140f8a28627103515505f32
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-10 10:40:53 -0400

    Created a tenary operator for if the acive song is the song then we want to show the pause icon else we want to show the play icon

--- spotify-clone/src/components/playpause/PlayPause.js
+++ spotify-clone/src/components/playpause/PlayPause.js
@@ -3,7 +3,7 @@ import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
 
 const PlayPause = ({ song, handlePause, handlePlay, isPlaying, activeSong }) => {
   return (
-    <div>PlayPause</div>
+    <div>{isPlaying && activeSong?.title === song.title ? <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} /> : <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />}</div>
   )
 }
 
