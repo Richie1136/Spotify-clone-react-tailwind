388582d2454a8f75442df35664356d648c72ad41
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-10 11:21:24 -0400

    Styling the SideBar component and added an image to the sidebar

--- spotify-clone/src/components/sidebar/SideBar.js
+++ spotify-clone/src/components/sidebar/SideBar.js
@@ -1,7 +1,22 @@
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import { RiCloseLine } from 'react-icons/ri'
+// import { logo } from '../../assets/logo.svg'
+import musicLogo from '../../assets/music-logo.jpeg'
+import links from '../../assets/constants'
+
 
 const SideBar = () => {
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+
   return (
-    <div>SideBar</div>
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img className="w-full h-14 object-contain" src={musicLogo} alt='logo' />
+      </div>
+    </>
   )
 }
 
