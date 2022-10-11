3be97f3c18e1569e7b176d46f8558820294ae2be
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-11 10:24:58 -0400

    Importing functions and topchart query from the redux folder and importing packages from swiper

--- spotify-clone/src/components/topplay/TopPlay.js
+++ spotify-clone/src/components/topplay/TopPlay.js
@@ -2,9 +2,20 @@ import { useEffect, useRef } from "react"
 import { NavLink } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode } from "swiper"
+import PlayPause from "../playpause/PlayPause"
+import { playPause, setActiveSong } from "../../redux/features/playerSlice"
+import { useGetTopChartsQuery } from '../../redux/services/shazamCore'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
 
 
 const TopPlay = () => {
+
+  const dispatch = useDispatch()
+
+
   return (
     <div>TopPlay</div>
   )
