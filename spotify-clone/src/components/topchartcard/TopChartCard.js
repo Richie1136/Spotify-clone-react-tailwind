a78408fd31c5736b3cee96b9c6f689910fabd49b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-12 10:30:04 -0400

    Rendering out the subtitle underneath the title and created a link to the artist detail page

--- spotify-clone/src/components/topchartcard/TopChartCard.js
+++ spotify-clone/src/components/topchartcard/TopChartCard.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
 
 const TopChartCard = ({ song, index }) => {
+  console.log(song)
   return (
     <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
       <h3 className="font-bold text-base text-white mr-3">{index + 1}.</h3>
@@ -9,7 +10,9 @@ const TopChartCard = ({ song, index }) => {
         <div className="flex-1 flex flex-col justify-center mx-3">
           <NavLink to={`/songs/${song.key}`}>
             <p className="text-xl font-bold text-white">{song?.title}</p>
-
+          </NavLink>
+          <NavLink to={`/artists/${song?.artists[0].adamid}`}>
+            <p className="text-xl font-bold text-white">{song?.subtitle}</p>
           </NavLink>
         </div>
       </div>
