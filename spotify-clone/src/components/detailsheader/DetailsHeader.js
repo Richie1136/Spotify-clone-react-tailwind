eeb11cd46219bef33d3deca440f16d8701db81c6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-13 11:49:31 -0400

    Changing the height and the width of the image to be 500 px

--- spotify-clone/src/components/detailsheader/DetailsHeader.js
+++ spotify-clone/src/components/detailsheader/DetailsHeader.js
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom"
 
 const DetailsHeader = ({ artistId, songData, artistData }) => {
+  console.log(artistData)
   return (
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+      <div className="absolute inset-0 flex items-center">
+        <img src={artistId ? artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart} alt='art' />
+      </div>
     </div>
   )
 }
