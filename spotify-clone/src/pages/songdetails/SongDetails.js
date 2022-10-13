80f87a48cb2505e676f61e4edad8733a88c61e6b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-13 10:21:42 -0400

    Added songDetails api route and using that query inside of the SongDetails component

--- spotify-clone/src/pages/songdetails/SongDetails.js
+++ spotify-clone/src/pages/songdetails/SongDetails.js
@@ -2,23 +2,26 @@ import { useParams } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../../components"
 import { setActiveSong, playPause } from "../../redux/features/playerSlice"
+import { useGetSongDetailsQuery } from "../../redux/services/shazamCore"
 
 const SongDetails = () => {
 
   const dispatch = useDispatch()
-  const params = useParams()
+  const songid = useParams()
 
-  const obj = new URLSearchParams(params)
+  const obj = new URLSearchParams(songid)
 
   const term = obj.get('songid')
   console.log(term)
 
   const { activeSong, isPlaying } = useSelector((state) => state.player)
 
+  const { data: songData, isFetching } = useGetSongDetailsQuery(songid)
+
 
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <DetailsHeader artistId={artistId} songData={songData} />
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
         <div className="mt-5">
