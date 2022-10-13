b728f4266cf711185b638e449414d764724514a1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-13 11:57:49 -0400

    Rendering out the song name

--- spotify-clone/src/components/detailsheader/DetailsHeader.js
+++ spotify-clone/src/components/detailsheader/DetailsHeader.js
@@ -6,7 +6,10 @@ const DetailsHeader = ({ artistId, songData, artistData }) => {
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
       <div className="absolute inset-0 flex items-center">
-        <img src={artistId ? artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart} alt='art' />
+        <img className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black" src={artistId ? artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart} alt='art' />
+        <div className="ml-5">
+          <p className="font-bold sm:text-3xl text-xl text-white">{artistId ? artistData?.artists[artistId].attributes.name : songData?.title}</p>
+        </div>
       </div>
     </div>
   )
