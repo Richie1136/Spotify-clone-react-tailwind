7d7699e8b7df99216e2dde2637b07f2f7d9f430a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-13 10:26:27 -0400

    Checking to see if i have any lyrics and if i do then map over the lyrics and render out the line and if i dont have lyrics then rendering sorry no lyrics found

--- spotify-clone/src/pages/songdetails/SongDetails.js
+++ spotify-clone/src/pages/songdetails/SongDetails.js
@@ -21,10 +21,13 @@ const SongDetails = () => {
 
   return (
     <div className="flex flex-col">
-      <DetailsHeader artistId={artistId} songData={songData} />
+      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
         <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
+            <p>{line}</p>
+          )) : <p>Sorry, no lyrics found</p>}
         </div>
       </div>
     </div>
