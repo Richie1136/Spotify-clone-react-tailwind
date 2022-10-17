10e5f5a62b0a4af8bf834c47349ef9695e9b9c13
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-17 10:33:02 -0400

    Adding one to the index number so it starts at 1 and not zero and adding a image and replacing the height and width to be 125 px

--- spotify-clone/src/components/songbar/SongBar.js
+++ spotify-clone/src/components/songbar/SongBar.js
@@ -3,7 +3,12 @@ import PlayPause from "../playpause/PlayPause"
 
 const SongBar = ({ song, index, artistId, isPlaying, activeSong, handlePlay, handlePause }) => {
   return (
-    <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer `}>SongBar</div>
+    <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer`}>
+      <h3 className="font-bold text-base text-white mr-3">{index + 1}.</h3>
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <img className="w-20 h-20 rounded-lg" src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart} alt={song?.title} />
+      </div>
+    </div>
   )
 }
 
