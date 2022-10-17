41358efc22281c0bd5673974e2bfa54bcb40bf07
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-17 11:20:55 -0400

    Created a tenary operator if theres no artist id then i want to have a link to go to the song details page

--- spotify-clone/src/components/songbar/SongBar.js
+++ spotify-clone/src/components/songbar/SongBar.js
@@ -7,7 +7,26 @@ const SongBar = ({ song, index, artistId, isPlaying, activeSong, handlePlay, han
       <h3 className="font-bold text-base text-white mr-3">{index + 1}.</h3>
       <div className="flex-1 flex flex-row justify-between items-center">
         <img className="w-20 h-20 rounded-lg" src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart} alt={song?.title} />
+        <div className='flex-1 flex flex-col justify-center mx-3'>
+          {!artistId ? (
+            <NavLink to={`/songs/${song.key}`}>
+              <p className="text-xl font-bold text-white">
+                {song?.title}
+              </p>
+            </NavLink>
+          ) : (
+            <p className="text-xl font-bold text-white">
+              {song?.attributes?.name}
+            </p>
+          )}
+          <p className="text-base text-gray-300 mt-1">
+            {artistId ? song?.attributes?.albumName : song?.subtitle}
+          </p>
+        </div>
       </div>
+      {!artistId ? (
+        <PlayPause isPlaying={isPlaying} activeSong={activeSong} song={song} handlePlay={() => handlePlay(song, index)} handlePause={handlePause} />
+      ) : null}
     </div>
   )
 }
