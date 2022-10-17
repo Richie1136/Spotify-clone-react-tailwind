fd5403f571e2a4952caf22753b4367868f3429c7
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-17 11:31:57 -0400

    For the songBar key inside of the RelatedSongs component i am either getting the songs key or the artist key depending on what the data is

--- spotify-clone/src/components/relatedsongs/RelatedSongs.js
+++ spotify-clone/src/components/relatedsongs/RelatedSongs.js
@@ -1,10 +1,15 @@
 import SongBar from "../songbar/SongBar"
 
 
-const RelatedSongs = ({ data, isPlaying, activeSong, handlePause, handlePlay }) => {
+const RelatedSongs = ({ data, isPlaying, activeSong, handlePause, handlePlay, artistId }) => {
   return (
     <div className="flex flex-col">
-
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+      <div className="mt-6 w-full flex flex-col">
+        {data?.map((song, index) => (
+          <SongBar key={`${song.key}-${artistId}`} />
+        ))}
+      </div>
     </div>
   )
 }
