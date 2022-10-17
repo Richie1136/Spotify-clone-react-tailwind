19de867e3dea55ea0e93cae699e86c03a0bc5dea
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-17 11:59:16 -0400

    Created a new fetch endpoint for the application and used what i had in the song details and changed the code a little bit to make the artist details component

--- spotify-clone/src/pages/artistdetails/ArtistDetails.js
+++ spotify-clone/src/pages/artistdetails/ArtistDetails.js
@@ -1,13 +1,31 @@
-
-
+import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../../components"
+import { useGetArtistDetailsQuery } from "../../redux/services/shazamCore"
 
 const ArtistDetails = () => {
 
+  const { artistid } = useParams()
 
+  const obj = new URLSearchParams(artistid)
+
+  const term = obj.get('artistid')
+  console.log(term)
+
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
+
+  const { data: artistData, isFetching, error } = useGetArtistDetailsQuery(artistid)
+
+
+  if (isFetching || error) return <Loader title="Loading artist details" />
 
 
   return (
-    <div>ArtistDetails</div>
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistid} artistData={artistData} />
+      <RelatedSongs artistId={artistid} data={Object.values(artistData?.songs)} isPlaying={isPlaying} activeSong={activeSong} />
+    </div>
   )
 }
 
