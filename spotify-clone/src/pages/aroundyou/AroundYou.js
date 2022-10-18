4f0a32dc092b68f74314c9a1a81ca15b255901aa
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-18 11:31:04 -0400

    Mapping over the data and rendering out SongCard component

--- spotify-clone/src/pages/aroundyou/AroundYou.js
+++ spotify-clone/src/pages/aroundyou/AroundYou.js
@@ -33,7 +33,12 @@ const AroundYou = () => {
 
   return (
     <div className="flex flex-col">
-      <h2>Around you</h2>
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you</h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, index) => (
+          <SongCard key={song.key} song={song} isPlaying={isPlaying} activeSong={activeSong} data={data} index={index} />
+        ))}
+      </div>
     </div>
   )
 }
