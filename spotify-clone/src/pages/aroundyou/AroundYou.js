0738ae316b93b94261743ea13c8d550c3fb7432a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-18 10:34:48 -0400

    Importing components inside of the Around You component and importing hooks from react and react redux

--- spotify-clone/src/pages/aroundyou/AroundYou.js
+++ spotify-clone/src/pages/aroundyou/AroundYou.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import { Error, Loader, SongCard } from "../../components"
 
 const AroundYou = () => {
   return (
