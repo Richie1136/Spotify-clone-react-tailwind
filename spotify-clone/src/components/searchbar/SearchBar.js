e28391d0520847b7a235810419c163a6c3c2e9d8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-19 10:50:53 -0400

    Created a function to change the input value inside of the input box, another function to prevent the page for refreshing and created state and added onChange and value prop to the input element

--- spotify-clone/src/components/searchbar/SearchBar.js
+++ spotify-clone/src/components/searchbar/SearchBar.js
@@ -1,9 +1,28 @@
-
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { FiSearch } from 'react-icons/fi'
 
 const SearchBar = () => {
+
+  const [input, setInput] = useState("")
+
+  const handleInput = (event) => {
+    setInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setInput("")
+  }
+
+
   return (
-    <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
       <label htmlFor="search field" className="sr-only">Search All Songs</label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch className="w-6 h-6 ml-4" />
+        <input name="Search Field" autoComplete="off" id='search field' placeholder="Search" type='search' value={input} onChange={handleInput} />
+      </div>
     </form>
   )
 }
