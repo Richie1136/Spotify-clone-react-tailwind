adf888b0b075133344b3f9fc80454e951339b0d2
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-19 10:35:44 -0400

    Created a label that has the text of Search All songs

--- spotify-clone/src/components/searchbar/SearchBar.js
+++ spotify-clone/src/components/searchbar/SearchBar.js
@@ -2,7 +2,9 @@
 
 const SearchBar = () => {
   return (
-    <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">SearchBar</form>
+    <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+      <label htmlFor="search field" className="sr-only">Search All Songs</label>
+    </form>
   )
 }
 
