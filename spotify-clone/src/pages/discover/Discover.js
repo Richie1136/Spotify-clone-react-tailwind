b779c17b1d2b1da60c1e3f4606cabe3dd6d728bd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-19 11:31:49 -0400

    Able to select different song categories and the genre title changes above the songs

--- spotify-clone/src/pages/discover/Discover.js
+++ spotify-clone/src/pages/discover/Discover.js
@@ -1,24 +1,26 @@
 import { Error, Loader, SongCard } from "../../components"
 import { genres } from '../../assets/constants'
-import { useGetTopChartsQuery } from "../../redux/services/shazamCore"
+import { useGetSongsByGenreQuery } from "../../redux/services/shazamCore"
 import { useDispatch, useSelector } from "react-redux"
 import { selectGenreListId } from "../../redux/features/playerSlice"
 
 
 const Discover = () => {
 
-  const { data, isFetching, error } = useGetTopChartsQuery()
+
   const dispatch = useDispatch()
   const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player)
+  const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP')
 
-  const genreTitle = "Pop"
+
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title
 
   if (isFetching) return <Loader title="Loading Songs..." />
 
   if (error) return <Error />
 
   const handleSelectChange = (event) => {
-    selectGenreListId(event.target.value)
+    dispatch(selectGenreListId(event.target.value))
   }
 
   return (
